681475287700a4410502884e7a60da4cd1a1b4bb
Adam3s <aharbisher@gmail.com>
2020-01-05 17:26:24 +0000

    Deleted App.js from src

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,20 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <div className="App-playlist">
+
+        </div>
+      </div>
     </div>
   );
 }
